6ae52104480cf9c7470607a235ef56e6b9e83c46
takky1126 <I11416@nara.kosen-ac.jp>
2025-04-26 20:17:26 +0900

    方向性決定

--- react_test/src/main.tsx
+++ react_test/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import "./index.css";
 import { App } from "./App";
 import { Test } from "./test";
+import Comsumption from "./test"; // default export なら波かっこ不要
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/comsumption" element={<Comsumption />} /> {/* ← 追加！ */}
       </Routes>
     </BrowserRouter>
   </StrictMode>,
